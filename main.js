d22e12de8876d54d88592e09186d6e60853d2485
Siyona-Nanda <88610903+Siyona-Nanda@users.noreply.github.com>
2021-11-25 11:18:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.position(250, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+    tint(tint_color);
+
+
+}
+
+function take_snapshot() {
+    save("siyona.png");
+
+}
+
+function filter_tint() {
+    tint_color = document.getElementById("color_name").value;
+
+}
